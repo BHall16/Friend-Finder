46cd353eb46a6fbf0821eb3ac3e14c2dfa657e7a
Brandon <hall.brandon@hotmail.com>
2019-03-17 08:36:13 -0400

    added html and js files

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+
+    {
+        name: "Mike",
+        photo: "https://www.orbitmedia.com/wp-content/uploads/2016/02/kurt-edit-bio-large-400x400.jpg",
+        scores: [
+            "4",
+            "2",
+            "3",
+            "4",
+            "4",
+            "4",
+            "3",
+            "5",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Ashley",
+        photo: "https://uwzm.wiscweb.wisc.edu/wp-content/uploads/sites/246/2017/06/LauraMonahan-400x400.jpg",
+        scores: [
+            "3",
+            "2",
+            "5",
+            "3",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Lauren",
+        photo: "https://blogs-images.forbes.com/files/2016/06/Tori-Utley_avatar_1465320691-400x400.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Donny",
+        photo: "http://wp-architects.com/wp-content/uploads/2017/10/PEOPLE_Green-Terry-400x400.jpg",
+        scores: [
+            "5",
+            "4",
+            "4",
+            "2",
+            "2",
+            "1",
+            "2",
+            "5",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Stephen",
+        photo: "https://132q6j40a81047nmwg1az6v8-wpengine.netdna-ssl.com/wp-content/uploads/2017/02/tommy-400x400.jpg",
+        scores: [
+            "1",
+            "1",
+            "4",
+            "4",
+            "3",
+            "1",
+            "2",
+            "5",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Alex",
+        photo: "https://132q6j40a81047nmwg1az6v8-wpengine.netdna-ssl.com/wp-content/uploads/2013/09/rendell-1-400x400.jpg",
+        scores: [
+            "5",
+            "1",
+            "1",
+            "1",
+            "5",
+            "1",
+            "4",
+            "3",
+            "4",
+            "2"
+        ]
+    }
+];
+
+module.exports = friends;
